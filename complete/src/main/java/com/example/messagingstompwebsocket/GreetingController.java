993bb5377c3d8b085534c20d6f6d848890da29e9
Nabeel Tariq Bhatti <nabeel.tariq@starzplayarabia.com>
2024-02-20 14:17:04 +0500

    web socket with unique socket id

--- complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -5,20 +5,24 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 
-@RestController
+@Controller
 public class GreetingController {
-	@Autowired
-	SimpMessagingTemplate simpMessagingTemplate;
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
 
-	@MessageMapping("/hello")
-	public void greeting(HelloMessage message) throws Exception {
-		Thread.sleep(2000); // simulated delay
-		System.out.println(message.getSocketId());
-		Greeting greeting = new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!", message.getSocketId());
-		simpMessagingTemplate.convertAndSend("/topic/greetings/"+message.getSocketId(), greeting);
-	}
+
+    @MessageMapping("/hello")
+    public void greeting(HelloMessage message) throws Exception {
+        Thread.sleep(2000); // simulated delay
+        System.out.println(message.getSocketId());
+        Greeting greeting = new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!", message.getSocketId());
+
+		System.out.println(Thread.currentThread().getName());
+        simpMessagingTemplate.convertAndSend("/topic/greetings/" + message.getSocketId(), greeting);
+    }
 
 }
--- complete/src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessagingStompWebsocketApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
 	}
